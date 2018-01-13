3da606f3049ea54d6d0b61e580020921f4b279a9
Bailey Matthews <Baileyjmatthews@gmail.com>
2018-01-13 12:03:41 +0000

    Removed HTML Head

--- config.php
+++ config.php
@@ -30,21 +30,3 @@ $surl = "https://hlx.bailey.guru";
 // Admin password for accepting or rejecting site
 $password = "1234";
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="language" content="en-gb"/>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="keywords" content="site,top,link,exchange,heliohost,helionet,atoz.ml,mrj,meraj"/>
-<meta name="description" content="<?=$dc?>"/>
-<link rel="stylesheet" href="http://<?=$surl?>/styles/materialize.min.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="http://<?=$surl?>/material-icons.min.css">
-<link rel="stylesheet" href="http://<?=$surl?>/style.css" type="text/css">
-<link rel="shortcut icon" href="http://<?=$surl?>/favicon.ico" type="image/x-icon"/>
-<link rel="icon" href="http://<?=$surl?>/favicon.ico" type="image/x-icon"/>
-<meta name="title" content="<?=$sname?> - <?=$title?>"/>
-<title><?=$sname?> - <?=$title?></title>
-</head>
-<body>
