afb536060198775b05923b62bf58b77775416437
Michael Wilcox <miwilc@gmx.com>
2018-01-14 17:37:25 -0500

    Cleaned up.
    Original attribution should be in the README. see issue #8

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -8,19 +8,17 @@
               <li><a href="https://heliohost.org">HelioHost</a></li>
               <li><a href="https://helionet.org">HelioNet</a></li>
               <li><a href="https://github.com/HelioNetworks/">HelioNetworks</a></li>
-              <li><a href="https://github.com/HelioNetworks/HelioLinkExchange">Github Repo</a></li>
+              <li><a href="https://github.com/HelioNetworks/HelioLinkExchange">Fork on GitHub</a></li>
               <li><a href="https://www.heliohost.org/donate/">Donate</a></li>
             </ul>
             <p>Made by <a href="https://www.helionet.org/index/user/123463-bailey/">Bailey Matthews</a>, <a href="https://www.helionet.org/index/user/96985-miwilc/">Michael Wilcox</a> and <a href="https://github.com/HelioNetworks/HelioLinkExchange/graphs/contributors">others</a>.</p>
-            <p>Code released under the <a href="https://github.com/HelioNetworks/HelioLinkExchange/blob/master/LICENSE">
-GNU Affero General Public License v3.0</a>.</p>
-            <p>Based on <a href="https://github.com/HelioNetworks/HelioLinkExchange/tree/original">the original work</a> proposed by <a href="https://www.helionet.org/index/user/118243-mrj/">mrj</a> on <a href="https://www.helionet.org/index/topic/30956-traffic-exchange/">this topic</a>.</p>
-            <p>Theme by <a href="https://bootswatch.com" rel="nofollow">Bootswatch</a>. Web fonts from <a href="https://fonts.google.com/" rel="nofollow">Google</a>.</p>
+            <p>Code released under the <a href="https://github.com/HelioNetworks/HelioLinkExchange/">AGPL3 or later</a>.</p>
+            <p>Theme by <a href="https://bootswatch.com" rel="nofollow">Bootswatch</a>.</p>
 
           </div>
         </div>
 
-      </footer>
+</footer>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
